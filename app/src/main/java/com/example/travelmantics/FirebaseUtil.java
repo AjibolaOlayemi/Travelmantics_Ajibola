4915658489528e27c663d91c3ad9c707541f3813
Ajibola Olayemi <Olayemi.emmanuel@rocketmail.com>
2020-07-16 12:17:59 +0100

    Initial commit

---
+++ app/src/main/java/com/example/travelmantics/FirebaseUtil.java
@@ -0,0 +1,24 @@
+package com.example.travelmantics;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class FirebaseUtil {
+    public static FirebaseDatabase mFirebaseDatabase;
+    public static DatabaseReference mDatabaseReference;
+    private static FirebaseUtil FirebaseUtil;
+    public static ArrayList <TravelDeal> mDeals;
+
+    private FirebaseUtil (){}
+
+    public static void openFBReference(String ref) {
+        if (FirebaseUtil == null) {
+            FirebaseUtil = new FirebaseUtil();
+            mFirebaseDatabase = FirebaseDatabase.getInstance();
+            mDeals = new ArrayList<TravelDeal>();
+        }
+        mDatabaseReference = mFirebaseDatabase.getReference().child(ref);
+    }
+}
